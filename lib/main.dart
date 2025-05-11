5977cfdaecfc3a85e64e9ce5368d1012a8f0feb1
Yago Alonso <yagoalonso3105@gmail.com>
2025-05-11 22:09:59 +0200

    fix:main

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'viewmodel/viewmodel.dart';
+import 'view/character_list_screen.dart';
 
+// Función principal que inicia la aplicación
 void main() {
-  runApp(const MainApp());
+  runApp(const MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+// Widget principal de la aplicación
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    // Provider: permite que el ViewModel esté disponible en toda la app
+    return ChangeNotifierProvider(
+      create: (_) => mainViewModel(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'HogwAPI',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightBlue),
+          useMaterial3: true,
+          primarySwatch: Colors.blue,
+          brightness: Brightness.light,
+          scaffoldBackgroundColor: Colors.white,
+          textTheme: const TextTheme(
+            headlineSmall: TextStyle(
+              color: Colors.black87,
+              fontWeight: FontWeight.bold,
+            ),
+            bodyLarge: TextStyle(color: Colors.black87),
+            bodyMedium: TextStyle(color: Colors.black87),
+          ),
+          cardTheme: CardTheme(
+            color: Colors.lightBlue[100],
+            elevation: 4,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(15),
+            ),
+          ),
         ),
+        home: const CharacterListScreen(),
       ),
     );
   }
